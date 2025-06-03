7a5587e5741fba696493e95ef0e9c260a2a49a5b
karroumi23 <anasskarroumi2016@gmail.com>
2025-06-03 15:56:24 +0200

    change (redirection) when the user logout take him to home-page

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -28,7 +28,7 @@ class AuthController {
      public function logout()
       {     
          session_destroy();
-         header('Location: index.php?action=login');
+         header('Location: index.php?action=home');
          exit;
       }  
    
